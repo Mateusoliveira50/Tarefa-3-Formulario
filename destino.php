be8e8b89552cf291a8d97e8865131d711c24a921
Mateus Oliveira <98608380+Mateusoliveira50@users.noreply.github.com>
2022-10-17 23:15:42 -0300

    Add files via upload

---
+++ destino.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boas Vindas</title>
+    <style>
+        body {
+        background-color: purple;
+        }
+        div{
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        color: white;
+        }
+ </style>
+</head>
+<body>
+    <div>
+    <?php
+    echo "<h1>Bem Vindo!</h1>"
+    ?>
+    </div>
+</body>
+</html>
