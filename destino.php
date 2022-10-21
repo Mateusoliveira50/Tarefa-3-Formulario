da9f3555c50eafb1a964faea6718a2ec9b7180e6
Mateusoliveira50 <mateusoliveira6@yahoo.com>
2022-10-21 00:42:58 -0300

    Alteração CSS

--- destino.php
+++ destino.php
@@ -5,24 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Boas Vindas</title>
-    <style>
-        body {
-        background-color: purple;
-        }
-        div{
-        font-family: Arial, Helvetica, sans-serif;
-        text-align: center;
-        color: white;
-        position: absolute;
-        top: 50%;
-        left: 15%;
-        transform: translate(-50%, -50%);
-        font-size: 45px;
-        }
- </style>
+<link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <div>
+<body class="corpo">
+    <div class="box">
     <?php
     echo "<h1>Bem Vindo!</h1>"
     ?>
